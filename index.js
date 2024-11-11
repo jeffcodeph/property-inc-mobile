86ecaa82a1730fb7a4fcc5dfd540a1915ee9d339
Vinz Jay Stone <71942876+vinzjaystone@users.noreply.github.com>
2024-11-11 10:14:29 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import React from "react";
+
 export const applyCustomCode = (externalCodeSetup: any) => {
 	// call custom code api here
+	externalCodeSetup.indexJsApi.addIndexJsFunction(() => {
+		console.log("function from external set upo");
+	});
+	
+	externalCodeSetup.cssApi.addGlobalStyle("container", {
+		backgroundColor: "red"
+	});
 };
