adad64b1e6ca5b27e429f1aa8e3d6a5c767a672b
Vinz Jay Stone <71942876+vinzjaystone@users.noreply.github.com>
2024-11-11 10:35:46 +0800

    Try replace messages screen

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ import React from "react";
 
 export const applyCustomCode = (externalCodeSetup: any) => {
 	// call custom code api here
-	externalCodeSetup.indexJsApi.addIndexJsFunction(() => {
+	externalCodeSetup.indexJsApi.addIndexJsFunction(async () => {
 		console.log("function from external set upo");
 	});
 	
 	externalCodeSetup.cssApi.addGlobalStyle("container", {
 		backgroundColor: "red"
 	});
+	
+	externalCodeSetup.navigationApi.replaceScreenComponent("messages", () => (
+		<View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+			<Text>Hello from custom Single Message Screen</Text>
+		</View>
+	));
 };
